93c05ad9ab0b2f4e65683a677430232c307ce2f2
KOTAROKASEKO <kase.ktr@gmail.com>
2025-01-22 21:09:32 +0800

    added user invitation function

--- lib/BottomTab.dart
+++ lib/BottomTab.dart
@@ -8,6 +8,7 @@ import 'package:rbl/home/PAGE_homePage.dart';
 import 'package:rbl/reservationService/ceilingTab.dart';
 import 'package:rbl/tabProviderService.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 class BottomTabView extends StatefulWidget {
   const BottomTabView({super.key});
@@ -23,6 +24,12 @@ class _BottomTabViewState extends State<BottomTabView> {
     GlobalKey<NavigatorState>(),
     GlobalKey<NavigatorState>(),
     GlobalKey<NavigatorState>(),
+  ];
+
+  List<String>linkLists = [
+    'https://qyvladzxxzhsacvdmyft.supabase.co/storage/v1/object/public/images/uploads/1737549214800',//20%
+    'https://qyvladzxxzhsacvdmyft.supabase.co/storage/v1/object/public/images/uploads/1737549265697',//50%
+    'https://qyvladzxxzhsacvdmyft.supabase.co/storage/v1/object/public/images/uploads/1737549312727',//80%
   ];
 
   @override
@@ -47,7 +54,8 @@ class _BottomTabViewState extends State<BottomTabView> {
         List<dynamic> invitationList = data['invitationList'];
         if (invitationList.isNotEmpty && mounted) {
           if(invitationList.length==3||invitationList.length==6||invitationList.length==9){
-            if(await checkIfItshouldBeShown(prefs, invitationList.length)){
+            if(
+              await doesShowNotification(prefs, invitationList.length)){
               prefs.setInt('alreadyShow',invitationList.length);
               showDialog(
                     context: context,
@@ -72,19 +80,7 @@ class _BottomTabViewState extends State<BottomTabView> {
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                   children: [
-                                    GestureDetector(
-                                      onTap:(){
-                                        Navigator.pop(context);
-                                      },
-                                    child:Padding(
-                                        padding: const EdgeInsets.symmetric(vertical: 15),
-                                        child:Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10.0),
-                                        color: const Color.fromARGB(255, 236, 219, 160)
-                                      ),
-                                      child: const Padding(padding: EdgeInsets.symmetric(horizontal: 10), child:Text('close'),)
-                                    ),),),
+                                    
 
                                     GestureDetector(
                                       onTap: () {
@@ -100,7 +96,7 @@ class _BottomTabViewState extends State<BottomTabView> {
                                           ),
                                           child: const Padding(
                                             padding: EdgeInsets.symmetric(horizontal: 10),
-                                            child: Text('claim'),
+                                            child: Text('OK'),
                                           ),
                                         ),
                                       ),
@@ -125,33 +121,90 @@ class _BottomTabViewState extends State<BottomTabView> {
     }
   }
 
-  Future<bool> checkIfItshouldBeShown(SharedPreferences pref, int NumberOfInvitedPpl) async{
+  Future<bool> doesShowNotification(SharedPreferences pref, int NumberOfInvitedPpl) async{
     int? lasttime = pref.getInt('alreadyShow');
+    print('record of invitation is ${lasttime}');
     if(lasttime==null){
+      generateInvitationCoupon(NumberOfInvitedPpl);
       return true;
     }else if(NumberOfInvitedPpl == lasttime){
-      return false;
+      return false; 
     }else if(NumberOfInvitedPpl != lasttime){
-      return true;
+      generateInvitationCoupon(NumberOfInvitedPpl);
+      return true; 
     }else{
-      return false;
+      return false; 
     }
   }
-  
-  // Function to handle tab change
-void _onItemTapped(int index) {
-  var tabProviderInstance = Provider.of<Tabproviderservice>(context, listen: false);
-  if (tabProviderInstance.getTabIndex() != index) {
-    setState(() {
-      _selectedIndex = index; // Update the local state
-    });
-    tabProviderInstance.changeIndex(index); // Update the Provider state
-  } else {
-    _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
+
+  Future<void> generateInvitationCoupon(NumberOfInvitedPpl)async{
+    try {
+      print('generateInvitationCoupon');
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    // Reference to the user's point document
+    
+    //===============================for individual =============
+    
+    Uuid uuid = Uuid();
+    String couponId = uuid.v4();
+
+    await firestore
+      .collection('userData')
+      .doc(AccountId.userId)
+      .collection('coupons')
+      .doc(couponId)
+      .set({'isUsed':false});
+    //===========================coupon data==========================
+    
+    //userData/couponId/isUsed
+    //coupons/couponId/details
+
+    Map<String, dynamic> individualCouponData = {
+      'isForEveryone':false,
+      'imageUrl': getDetail(NumberOfInvitedPpl).imageUrl,
+      'couponId':couponId,
+      'description':'RBL with you',
+      'discountValue': getDetail(NumberOfInvitedPpl).value,
+      'discountType':'subtraction',
+      'validUntil': DateTime.now().add(const Duration(days: 30)),
+      'whoUsed':[],
+    };
+    await firestore
+      .collection('coupons')
+      .doc(couponId)
+      .set(individualCouponData);
+
+      print('coupon generated. the id is : $couponId');
+    
+  } catch (e) {
+    print("Failed to update points: $e");
   }
 }
+  
+  Detail getDetail(int NumberOfInvitedPpl){
+    if(NumberOfInvitedPpl==3){
+        return Detail(imageUrl: linkLists[0], value: 10);
+      }else if(NumberOfInvitedPpl==6){
+        return Detail(imageUrl: linkLists[1], value: 20);
+      }else{
+        return Detail(imageUrl: linkLists[2], value: 30);
+    }
+  }
 
 
+  // Function to handle tab change
+  void _onItemTapped(int index) {
+    var tabProviderInstance = Provider.of<Tabproviderservice>(context, listen: false);
+    if (tabProviderInstance.getTabIndex() != index) {
+      setState(() {
+        _selectedIndex = index; // Update the local state
+      });
+      tabProviderInstance.changeIndex(index); // Update the Provider state
+    } else {
+      _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
+    }
+  }
 
   Future<bool> _onWillPop() async {
     final isFirstRouteInCurrentTab =
@@ -236,4 +289,10 @@ Widget build(BuildContext context) {
     ),
   );
 }
+}
+
+class Detail{
+  String imageUrl;
+  int value;
+  Detail({required this.imageUrl,required this.value});
 }
--- lib/coupon/PAGE_redeemCoupon.dart
+++ lib/coupon/PAGE_redeemCoupon.dart
@@ -140,6 +140,7 @@ class redeemDetailState extends State<redeemDetail> {
   }
 }
 
+  
 
 
   @override
